90f984f51be1a4f8ed3a9b37d3d2524a5a26f413
Gerigon <Rutger-100@hotmail.com>
2015-10-11 22:00:11 +0200

    Circle Motion Jerk

---
+++ Assets/Scripts/MotionDetection.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotionDetection : MonoBehaviour {
+
+    public GameObject indicationMarker;
+    public float MultiplacationOffset = 0.25f;
+    public float PlusOffset = 0.4f;
+    public float HeightOffset = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("objects spawned");
+        Instantiate(indicationMarker, new Vector3(transform.position.x - PlusOffset, transform.position.y + (transform.GetComponent<CapsuleCollider>().bounds.size.y / 2) + HeightOffset, transform.position.z), Quaternion.identity);
+        Instantiate(indicationMarker, new Vector3(transform.position.x + PlusOffset, transform.position.y + (transform.GetComponent<CapsuleCollider>().bounds.size.y / 2) + HeightOffset, transform.position.z), Quaternion.identity);
+        Instantiate(indicationMarker, new Vector3(transform.position.x , transform.position.y + (transform.GetComponent<CapsuleCollider>().bounds.size.y / 2) + HeightOffset, transform.position.z -PlusOffset), Quaternion.identity);
+        Instantiate(indicationMarker, new Vector3(transform.position.x , transform.position.y + (transform.GetComponent<CapsuleCollider>().bounds.size.y / 2) + HeightOffset, transform.position.z +PlusOffset), Quaternion.identity);
+        Instantiate(indicationMarker, new Vector3(transform.position.x - MultiplacationOffset, transform.position.y + (transform.GetComponent<CapsuleCollider>().bounds.size.y / 2) + HeightOffset, transform.position.z + MultiplacationOffset), Quaternion.identity);
+        Instantiate(indicationMarker, new Vector3(transform.position.x - MultiplacationOffset, transform.position.y + (transform.GetComponent<CapsuleCollider>().bounds.size.y / 2) + HeightOffset, transform.position.z - MultiplacationOffset), Quaternion.identity);
+        Instantiate(indicationMarker, new Vector3(transform.position.x + MultiplacationOffset, transform.position.y + (transform.GetComponent<CapsuleCollider>().bounds.size.y / 2) + HeightOffset, transform.position.z + MultiplacationOffset), Quaternion.identity);
+        Instantiate(indicationMarker, new Vector3(transform.position.x + MultiplacationOffset, transform.position.y + (transform.GetComponent<CapsuleCollider>().bounds.size.y / 2) + HeightOffset, transform.position.z - MultiplacationOffset), Quaternion.identity);
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+	    
+	}
+}
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour
+{
+
+    public GameObject indicationMarker;
+    public GameObject tempMarker;
+    public float MultiplacationOffset = 0.25f;
+    public float PlusOffset = 0.4f;
+    public float HeightOffset = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("objects spawned");
+
+        tempMarker = Instantiate(indicationMarker, new Vector3(transform.position.x - (PlusOffset * transform.localScale.x), transform.position.y + (transform.GetComponent<CapsuleCollider>().bounds.size.y / 2) + HeightOffset * transform.localScale.x, transform.position.z), Quaternion.identity)as GameObject;
+        tempMarker.transform.localScale = transform.localScale/2;
+        tempMarker.transform.parent = transform;
+        tempMarker.transform.localScale = transform.localScale;
+        tempMarker = Instantiate(indicationMarker, new Vector3(transform.position.x + (PlusOffset * transform.localScale.x), transform.position.y + (transform.GetComponent<CapsuleCollider>().bounds.size.y / 2) + HeightOffset * transform.localScale.x, transform.position.z), Quaternion.identity) as GameObject;
+        tempMarker.transform.localScale = transform.localScale;
+        tempMarker.transform.parent = transform;
+        tempMarker = Instantiate(indicationMarker, new Vector3(transform.position.x, transform.position.y + (transform.GetComponent<CapsuleCollider>().bounds.size.y / 2) + (HeightOffset * transform.localScale.x), transform.position.z - (PlusOffset * transform.localScale.x)), Quaternion.identity) as GameObject;
+        tempMarker.transform.localScale = transform.localScale;
+        tempMarker.transform.parent = transform;
+        tempMarker = Instantiate(indicationMarker, new Vector3(transform.position.x, transform.position.y + (transform.GetComponent<CapsuleCollider>().bounds.size.y / 2) + (HeightOffset * transform.localScale.x), transform.position.z + (PlusOffset * transform.localScale.x)), Quaternion.identity) as GameObject;
+        tempMarker.transform.localScale = transform.localScale;
+        tempMarker.transform.parent = transform;
+        tempMarker = Instantiate(indicationMarker, new Vector3(transform.position.x - (MultiplacationOffset * transform.localScale.x), transform.position.y + (transform.GetComponent<CapsuleCollider>().bounds.size.y / 2) + (HeightOffset * transform.localScale.x), transform.position.z + (MultiplacationOffset * transform.localScale.x)), Quaternion.identity) as GameObject;
+        tempMarker.transform.localScale = transform.localScale;
+        tempMarker.transform.parent = transform;
+        tempMarker = Instantiate(indicationMarker, new Vector3(transform.position.x - (MultiplacationOffset * transform.localScale.x), transform.position.y + (transform.GetComponent<CapsuleCollider>().bounds.size.y / 2) + (HeightOffset * transform.localScale.x), transform.position.z - (MultiplacationOffset * transform.localScale.x)), Quaternion.identity) as GameObject;
+        tempMarker.transform.localScale = transform.localScale;
+        tempMarker.transform.parent = transform;
+        tempMarker = Instantiate(indicationMarker, new Vector3(transform.position.x + (MultiplacationOffset * transform.localScale.x), transform.position.y + (transform.GetComponent<CapsuleCollider>().bounds.size.y / 2) + (HeightOffset * transform.localScale.x), transform.position.z + (MultiplacationOffset * transform.localScale.x)), Quaternion.identity) as GameObject;
+        tempMarker.transform.localScale = transform.localScale;
+        tempMarker.transform.parent = transform;
+        tempMarker = Instantiate(indicationMarker, new Vector3(transform.position.x + (MultiplacationOffset * transform.localScale.x), transform.position.y + (transform.GetComponent<CapsuleCollider>().bounds.size.y / 2) + (HeightOffset * transform.localScale.x), transform.position.z - (MultiplacationOffset * transform.localScale.x)), Quaternion.identity) as GameObject;
+        tempMarker.transform.localScale = transform.localScale;
+        tempMarker.transform.parent = transform;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
